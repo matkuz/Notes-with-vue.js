fa7ad54448235a2d88f7216df5a0e03c1297a93b
matkuz <mateusz.k93@gmail.com>
2016-12-22 12:02:48 +0100

    edit notes v.1.0

--- js/site/app.js
+++ js/site/app.js
@@ -1,106 +1,64 @@
-/*rwd hover nav*/
-if ($(window).width() <= 992) {  
-        $("#sidr-main-nav ul > li a").addClass("hvr-underline-from-center");
-    }else{
-        $("#sidr-main-nav ul > li a").removeClass("hvr-underline-from-center");
-    };
-
-    $(window).resize(function(){
-           if ($(window).width() <= 992) {  
-                $("#sidr-main-nav ul > li a").addClass("hvr-underline-from-center");
-           }else{
-                $("#sidr-main-nav ul > li a").removeClass("hvr-underline-from-center");
-           };
+var vm = new Vue({
+        el: '#vue-app',
+        data: {
+            n : localStorage.length,
+            notes: [],
+            display_notes_status : 0,
+            note_is_edited: false,
+            key_of_note_to_edit: 0,
+            exist_notes: false
+        },
+        methods: {
+            new_note: function(){
+                localStorage.setItem(this.n , this.text_of_note);//save note in localSorage
+                this.notes.push(this.text_of_note);//adding to display notes
+                this.n = this.n + 1;
+                this.text_of_note = "";
+            },
+            display_notes: function(){
+                /* display notes after click btn ( only once possibility ) */
+                if(this.display_notes_status == 0){
+                    this.notes = [];
+                    for (var i = 0; i < localStorage.length; i++){
+                        this.notes.push(localStorage.getItem(i));
+                    }
+                    this.display_notes_status = 1;
+                    $(".display-notes-btn").hide("slow");
+                }else{
+                    alert("Notatki są już wyświetlone lub nie dodałeś jeszcze ani jednej ;)");
+                }
+            },
+            delete_note: function(key){
+                localStorage.removeItem(key);
+                this.notes = [];
+                for (var i = 0; i < localStorage.length; i++){
+                    this.notes.push(localStorage.getItem(i));
+                }
+                this.n = this.n - 1;
+            },
+            edit_note: function(key_of_note){
+                this.note_is_edited = !this.note_is_edited;
+                if(this.note_is_edited == true){
+                    this.note_to_edit = this.notes[key_of_note];
+                    this.key_of_note_to_edit = key_of_note;
+                }
+            },
+            save_edit_note: function(){
+                localStorage.setItem(this.key_of_note_to_edit , this.text_of_edit_note);
+                this.notes = [];
+                for (var i = 0; i < localStorage.length; i++){
+                    this.notes.push(localStorage.getItem(i));
+                }
+                this.note_is_edited = !this.note_is_edited;
+            }
+        },
+        computed: {
+            exist_notes: function(){
+                if(localStorage.length == 0){
+                    return false;
+                }else{
+                    return true;
+                }
+            }
+        }
     });
-
-/*nav*/
-
-var scrollingStartDistance = $("nav").offset().top;
-    $(document).scroll(function () {
-        var scrollTop = $(document).scrollTop();
-                $("#top-wrapper").toggleClass("top-wrapperScroll", scrollTop > scrollingStartDistance);
-    });
-/*tooltip*/
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
-});
-/*sidr*/
-
-$(function() {
-    $('#sidr-toggle').sidr({
-        name: 'sidr-nav',
-        source: '#sidr-main-nav',
-        side: 'left',
-        renaming: 0,
-        speed: 400,
-    });
-
-    $('body>.wrapper, .sidr a.onepage').on('click', function() {
-        $.sidr('close', 'sidr-nav');
-        $('a.sidr-toggle .tr').removeClass('active');
-    });
-
-    // transformicon animation
-    $('a.sidr-toggle').on('click', function(){
-        $(this).find('.tr').toggleClass('active');
-    });
-});
-
-/*slider*/
-
-var owl = $('#owl-main-slider');
-                        owl.owlCarousel({
-                            itemElement: 'slide',
-                            items: 1,
-                            autoplay: true, 
-                            autoplaySpeed: 800,
-                            autoplayTimeout: 5800,
-                            navSpeed: 1500,
-                            autoplayHoverPause: true,
-                            center: false,
-                            autoWidth: false,
-                            loop: true,
-                            rewind: false,
-                            mouseDrag: true,
-                            dots: true,
-                            nav: true,
-                            navText: [
-                              "<i class='fa fa-angle-left'></i>",
-                              "<i class='fa fa-angle-right'></i>"
-                            ],
-                            margin: 0,
-                            itemsDesktop : [1199,1],
-                            itemsDesktopSmall : [980,1],
-                            itemsTablet: [768,1],
-                            itemsMobile : [479,1]
-                        });
-
-//slider2
-
-var owl = $('#owl-offer-slider');
-                        owl.owlCarousel({
-                            itemElement: 'slide2',
-                            items: 1,
-                            autoplay: true, 
-                            autoplaySpeed: 800,
-                            autoplayTimeout: 5800,
-                            navSpeed: 1500,
-                            autoplayHoverPause: true,
-                            center: false,
-                            autoWidth: false,
-                            loop: true,
-                            rewind: false,
-                            mouseDrag: true,
-                            dots: true,
-                            dotsContainer: '#main-slider-dots',
-                            nav: true,
-                            navText: [
-                              "<i class='fa fa-angle-left'></i>",
-                              "<i class='fa fa-angle-right'></i>"
-                            ],
-                            margin: 0,
-                            itemsDesktop : [1199,1],
-                            itemsDesktopSmall : [980,1],
-                            itemsTablet: [768,1],
-                            itemsMobile : [479,1]
-                        });
